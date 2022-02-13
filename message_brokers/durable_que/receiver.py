d14afbe4d717412c92c7df575632c12c3aa1ba96
Patryk Lisik <lisikpatryk@gmail.com>
2022-02-13 09:59:23 +0100

    better logging

--- message_brokers/durable_que/receiver.py
+++ message_brokers/durable_que/receiver.py
@@ -5,7 +5,18 @@ from time import sleep
 
 import pika
 
-logging.basicConfig(level=logging.INFO)
+logFormatter = logging.Formatter("%(asctime)s-%(name)s-%(levelname)s-%(message)s")
+logger = logging.getLogger()
+
+fileHandler = logging.FileHandler("log.log")
+fileHandler.setFormatter(logFormatter)
+fileHandler.setLevel(logging.DEBUG)
+logger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+consoleHandler.setLevel(logging.INFO)
+logger.addHandler(consoleHandler)
 
 TASK_TIME_MIN = int(os.getenv("TASK_TIME_MIN"))
 TASK_TIME_MAX = int(os.getenv("TASK_TIME_MAX"))
@@ -15,33 +26,31 @@ def callback(ch, method, properties, body):
     # 10% chance to random fail
     random_exception = random.randint(0, 10)
     if random_exception == 1:
-        raise RuntimeError(f'Task {body} were unlucky')
-    logging.info(f"Received {body}")
+        raise RuntimeError(f'Task {body} was unlucky')
+    logger.info(f"Received {body}")
     sleep(random.randint(TASK_TIME_MIN, TASK_TIME_MAX))
-    logging.info(f"Processed {body}")
+    logger.info(f"Processed {body}")
     ch.basic_ack(delivery_tag=method.delivery_tag)
-    logging.info(f"Acknowledged {body}")
+    logger.info(f"Acknowledged {body}")
 
 
 def start_consumer():
-    logging.info("Connecting to que")
+    logger.info("Connecting to que")
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='que', blocked_connection_timeout=30))
     channel = connection.channel()
     channel.queue_declare(queue='hello', durable=True)
-    channel.basic_qos(prefetch_count=1)
+    channel.basic_qos(prefetch_count=10)
     channel.basic_consume(queue='hello', on_message_callback=callback)
-    logging.info('consuming starts')
+    logger.info('consuming starts')
     channel.start_consuming()
 
 
 if __name__ == '__main__':
-    logging.info("Consumer starts")
+    logger.info("Consumer starts")
     while True:
         try:
-            logging.info("Consumer starts")
+            logger.info("Consumer starts")
             start_consumer()
         except pika.exceptions.AMQPConnectionError:
-            logging.warning("que unavailable. Sleep for 5s")
+            logger.warning("que unavailable. Sleep for 5s")
             sleep(5)
-
-
